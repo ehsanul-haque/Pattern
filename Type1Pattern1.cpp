e835134533ef5ab3286eb07c650f654d3f2133fb
ehsanul-haque <mdehaquesiam@gmail.com>
2022-10-11 20:33:56 +0600

    Type1Pattern1-7

---
+++ Type1Pattern1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, row, col;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row++)
+    {
+        for (col = 1; col <= row; col++)
+        {
+            cout << " " << col;
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ Type1Pattern2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n, row, col;
+
+    cout << "Enter number of rows: ";
+    cin >> n;
+
+    for (row = 1; row <= n; row++)
+    {
+
+        for (col = 1; col <= row; col++)
+        {
+
+            cout << row;
+        }
+        cout << endl;
+    }
+    return 0;
+}
